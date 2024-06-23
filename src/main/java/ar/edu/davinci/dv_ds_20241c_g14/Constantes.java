76683dae38b11fddce57a32103d6ab84f1337802
MatiasSanchez <mati_sanchez23@hotmail.com>
2024-06-22 22:12:59 -0300

    agregado interface constantes, completamos clases en package mapper, controller.rest y controller.web

---
+++ src/main/java/ar/edu/davinci/dv_ds_20241c_g14/Constantes.java
@@ -0,0 +1,5 @@
+package ar.edu.davinci.dv_ds_20241c_g14;
+
+public interface Constantes {
+	public static final String FORMATO_FECHA = "dd-MM-yyyy";
+}
---
+++ src/main/java/ar/edu/davinci/dv_ds_20241c_g14/mapper/ClienteMapper.java
@@ -0,0 +1,21 @@
+package ar.edu.davinci.dv_ds_20241c_g14.mapper;
+
+import java.util.Collection;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import ar.edu.davinci.dv_ds_20241c_g14.controller.request.ClienteInsertRequest;
+import ar.edu.davinci.dv_ds_20241c_g14.controller.request.ClienteUpdateRequest;
+import ar.edu.davinci.dv_ds_20241c_g14.controller.response.ClienteResponse;
+import ar.edu.davinci.dv_ds_20241c_g14.domain.Cliente;
+
+@Mapper
+public interface ClienteMapper {
+	ClienteMapper instance = Mappers.getMapper(ClienteMapper.class);
+	ClienteResponse mapToClienteResponse(Cliente cliente);
+	Cliente matToCliente(ClienteInsertRequest clienteDto);
+	Cliente matToCliente(ClienteUpdateRequest clienteDto);
+	List<ClienteResponse> matToListClienteResponse(Collection<Cliente> clientes);
+}
+
